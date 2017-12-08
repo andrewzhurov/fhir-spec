92eeaebd52212db470a8c80ad5322ac62f18a988
Andrew Zhurov <brownmoose3q@gmail.com>
2017-12-08 14:36:27 +0300

    Dependency upgrade, add spec-tools

--- project.clj
+++ project.clj
@@ -5,5 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-RC2"]
                  [org.clojure/clojurescript "1.9.946"]
-                 [metosin/compojure-api "2.0.0-alpha15"]
-                 ])
+                 [metosin/compojure-api "2.0.0-alpha16"]
+                 [metosin/spec-tools "0.5.1"]])
